980985eb69c9e15c0ba5a2ca73a6a256c685402d
Tonghun Hwang <tonghun.hwang@sportwiss.uni-hannover.de>
2019-04-26 17:13:26 +0200

    load csound library
    Signed-off-by: tonghun-hwang <tonghun.hwang@sportwiss.uni-hannover.de>

--- SwiftSocket/CircleRun-Bridging-Header.h
+++ SwiftSocket/CircleRun-Bridging-Header.h
@@ -2,3 +2,13 @@
 //  Use this file to import your target's public headers that you would like to expose to Swift.
 //
 
+#ifndef CircleRun_Bridging_Header_h
+#define CircleRun_Bridging_Header_h
+
+#import "CsoundObj.h"
+#import "CsoundUI.h"
+#import "CsoundMotion.h"
+#import "CsoundMIDI.h"
+#import "MidiWidgetsManager.h"
+
+#endif /* CircleRun_Bridging_Header_h */
